6f7764afa234cb2f7e3641a30d3b89522ff827f1
vishxlkr <vishxlkr@gmail.com>
2025-07-19 13:01:55 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>hello world</div>;
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
